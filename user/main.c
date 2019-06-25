362bbb76d94a461efeee74acbaf3f180c83c0525
jirm2019 <51276412+jirm2019@users.noreply.github.com>
2019-06-24 18:08:59 -0700

    2019.06.24 Gimbal Control with ints

--- user/main.c
+++ user/main.c
@@ -1,12 +1,12 @@
 /**
   ****************************(C) COPYRIGHT 2016 DJI****************************
   * @file       main.c/h
-  * @brief      stm32��ʼ���Լ���ʼ����freeRTOS��h�ļ��������ȫ�ֺ궨���Լ�
-  *             typedef һЩ������������
+  * @brief      stm32初始化以及开始任务freeRTOS。h文件定义相关全局宏定义以及
+  *             typedef 一些常用数据类型
   * @note
   * @history
   *  Version    Date            Author          Modification
-  *  V1.0.0     Dec-26-2018     RM              1. ���
+  *  V1.0.0     Dec-26-2018     RM              1. 完成
   *
   @verbatim
   ==============================================================================
@@ -42,6 +42,7 @@
 #include "start_task.h"
 
 void BSP_init(void);
+void USART_setup(void);
 
 int main(void)
 {
@@ -55,46 +56,46 @@ int main(void)
     }
 }
 
-//�ĸ�24v ��� ���ο��� ��� 709us
+//四个24v 输出 依次开启 间隔 709us
 #define POWER_CTRL_ONE_BY_ONE_TIME 709
 
 void BSP_init(void)
 {
-    //�ж��� 4
+    //中断组 4
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-    //��ʼ���δ�ʱ��
+    //初始化滴答时钟
     delay_init(configTICK_RATE_HZ);
-    //��ˮ�ƣ����̵Ƴ�ʼ��
+    //流水灯，红绿灯初始化
     led_configuration();
-    //stm32 �����¶ȴ�������ʼ��
+    //stm32 板载温度传感器初始化
     temperature_ADC_init();
 #if GIMBAL_MOTOR_6020_CAN_LOSE_SLOVE
-    //stm32 �������������ʼ��
+    //stm32 随机数发生器初始化
     RNG_init();
 #endif
-    //24������ƿ� ��ʼ��
+    //24输出控制口 初始化
     power_ctrl_configuration();
-    //Ħ���ֵ��PWM��ʼ��
+    //摩擦轮电机PWM初始化
     fric_PWM_configuration();
-    //��������ʼ��
+    //蜂鸣器初始化
     //buzzer_init(30000, 90);
-    //����IO��ʼ��
+    //激光IO初始化
     laser_configuration();
-    //��ʱ��6 ��ʼ��
+    //定时器6 初始化
     TIM6_Init(60000, 90);
-    //CAN�ӿڳ�ʼ��
+    //CAN接口初始化
     CAN1_mode_init(CAN_SJW_1tq, CAN_BS2_2tq, CAN_BS1_6tq, 5, CAN_Mode_Normal);
     CAN2_mode_init(CAN_SJW_1tq, CAN_BS2_2tq, CAN_BS1_6tq, 5, CAN_Mode_Normal);
 
-    //24v ��� �����ϵ�
+    //24v 输出 依次上电
     for (uint8_t i = POWER1_CTRL_SWITCH; i < POWER4_CTRL_SWITCH + 1; i++)
     {
         power_ctrl_on(i);
         delay_us(POWER_CTRL_ONE_BY_ONE_TIME);
     }
-    //ң������ʼ��
+    //遥控器初始化
     remote_control_init();
-    //flash��ȡ��������У׼ֵ�Żض�Ӧ����
+    //flash读取函数，把校准值放回对应参数
     cali_param_init();
 
 
