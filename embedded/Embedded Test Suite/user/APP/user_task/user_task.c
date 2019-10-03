778ac4eeb35f88220530f9480929a0ce029408db
jirm2019 <jachnoin2@gmail.com>
2019-10-03 11:13:28 -0700

    Serial comms over USART port 6

--- embedded/Embedded Test Suite/user/APP/user_task/user_task.c
+++ embedded/Embedded Test Suite/user/APP/user_task/user_task.c
@@ -22,6 +22,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+/* Serial stuff */
+#include "stm32f4xx.h"
+void Serial_sendString(volatile char *str);
+/* End serial stuff */
+
 #include "led.h"
 
 #include "Detect_Task.h"
@@ -67,70 +72,15 @@ void UserTask(void *pvParameters)
     }
 }
 
-void Serial_setup(void)
-{
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure;
-
-	// Enable the GPIOG clock
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
-	// Enable peripheral clock for USART6
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
-
-	// Force USART6 clock to reset
-	RCC_APB2PeriphResetCmd(RCC_APB2Periph_USART6, ENABLE);
-	RCC_APB2PeriphResetCmd(RCC_APB2Periph_USART6, DISABLE);
-
-	// Setup GPIO pins for UART
-
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource14, GPIO_AF_USART6);	// Tx
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource9, GPIO_AF_USART6);		// Rx
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_9;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;				// rc USART uses GPIO_PuPd_NOPULL
-	GPIO_Init(GPIOG, &GPIO_InitStructure);
-
-	USART_DeInit(USART6);
-
-	// Configure UART - this all needs to match the config on the Pi
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;			// These are the default settings
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;					// All set by USART_StructInit()
-	USART_InitStructure.USART_Parity = USART_Parity_No;					// These details are here for clarity
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-
-
-	// USART_StructInit(&USART_InitStructure); 	// all the settings above are the default settings, this sets them all
-	USART_Init(USART6, &USART_InitStructure);
-
-	// enable interrupts
-	USART_ITConfig(USART6, USART_IT_RXNE, ENABLE);
-
-	// enable USART6 peripheral
-	USART_Cmd(USART6, ENABLE);
-
-	// configure NVIC for interrupts - only on Rx
-	NVIC_InitStructure.NVIC_IRQChannel = USART6_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;		// set priority group to highest priority achieveable by sw (hw can go negative)
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;					// set priority highest in subgroup
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_Init(&NVIC_InitStructure);
-
-}
 
 // Sends one string over USART
-// example usage: Serial_sendString(USART6, "Nando's Sauce ;)");
-void Serial_sendString(USART_TypeDef *USARTx, volatile char *str)
+// example usage: Serial_sendString("Nando eats Nando's at Nando's");
+void Serial_sendString(volatile char *str)
 {
 	while (*str) {
 		// Once previous byte is finished being transmitted, transmit next byte
-		while (USART_GetFlagStatus(*USART, USART_FLAG_TC) == RESET);
-		USART_SendData(USARTx, *str);
+		while (USART_GetFlagStatus(USART6, USART_FLAG_TC) == RESET);
+		USART_SendData(USART6, *str);
 		str++;
 	}
 }
--- embedded/Embedded Test Suite/user/main.c
+++ embedded/Embedded Test Suite/user/main.c
@@ -42,6 +42,7 @@
 #include "start_task.h"
 
 void BSP_init(void);
+void Serial_setup(void);
 
 int main(void)
 {
@@ -96,4 +97,62 @@ void BSP_init(void)
     remote_control_init();
     //flash读取函数，把校准值放回对应参数
     cali_param_init();
+		
+		Serial_setup();
+}
+
+void Serial_setup(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	// Enable the GPIOG clock
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
+	// Enable peripheral clock for USART6
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
+
+	// Force USART6 clock to reset
+	RCC_APB2PeriphResetCmd(RCC_APB2Periph_USART6, ENABLE);
+	RCC_APB2PeriphResetCmd(RCC_APB2Periph_USART6, DISABLE);
+
+	// Setup GPIO pins for UART
+
+	GPIO_PinAFConfig(GPIOG, GPIO_PinSource14, GPIO_AF_USART6);	// Tx
+	GPIO_PinAFConfig(GPIOG, GPIO_PinSource9, GPIO_AF_USART6);		// Rx
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;				// rc USART uses GPIO_PuPd_NOPULL
+	GPIO_Init(GPIOG, &GPIO_InitStructure);
+
+	USART_DeInit(USART6);
+
+	// Configure UART - this all needs to match the config on the Pi
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;			// These are the default settings
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;					// All set by USART_StructInit()
+	USART_InitStructure.USART_Parity = USART_Parity_No;					// These details are here for clarity
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+
+
+	// USART_StructInit(&USART_InitStructure); 	// all the settings above are the default settings, this sets them all
+	USART_Init(USART6, &USART_InitStructure);
+
+	// enable interrupts
+	USART_ITConfig(USART6, USART_IT_RXNE, ENABLE);
+
+	// enable USART6 peripheral
+	USART_Cmd(USART6, ENABLE);
+
+	// configure NVIC for interrupts - only on Rx
+	NVIC_InitStructure.NVIC_IRQChannel = USART6_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;		// set priority group to highest priority achieveable by sw (hw can go negative)
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;					// set priority highest in subgroup
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
 }
